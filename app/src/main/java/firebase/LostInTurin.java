e27bb17fb11c1e8385d53bd92e7d22249fd0d40d
Gregorio <greg.massara@gmail.com>
2018-07-16 23:11:42 +0100

    Firebase dB synced offline persistency for all the nodes.

--- app/src/main/java/firebase/LostInTurin.java
+++ app/src/main/java/firebase/LostInTurin.java
@@ -1,11 +1,19 @@
 package firebase;
 
 import android.app.Application;
-import com.example.gregorio.capstone.R;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class LostInTurin extends Application {
+
+  private static final String FIREBASE_URL = "https://turin-guide-1526861835739.firebaseio.com/";
+  private static final String FIREBASE_ROOT_NODE = "checkouts";
+  private static final String FIREBASE_FAVOURITE_CHILD_NODE_FAVOURITES = "Favourites";
+  private static final String FIREBASE_FAVOURITE_CHILD_NODE_SIGHTS = "sights";
+  private static final String FIREBASE_FAVOURITE_CHILD_NODE_MUSEUM = "museums";
+  private static final String FIREBASE_FAVOURITE_CHILD_NODE_FOOD = "food";
+  private static final String FIREBASE_FAVOURITE_CHILD_NODE_NIGHTLIFE = "nightlife";
+  private static final String FIREBASE_FAVOURITE_CHILD_NODE_DRINKS = "drinks";
 
   @Override
   public void onCreate() {
@@ -14,10 +22,8 @@ public class LostInTurin extends Application {
     // Enable disk persistence only once at the start of the app
     // and before any other Firebase instances
     FirebaseDatabase.getInstance().setPersistenceEnabled(true);
-    String favourite = getString(R.string.nv_favourites);
-
-    DatabaseReference favouritesRef = FirebaseDatabase.getInstance().getReference(favourite);
+    DatabaseReference favouritesRef = FirebaseDatabase.getInstance()
+        .getReference(FIREBASE_ROOT_NODE);
     favouritesRef.keepSynced(true);
-
   }
 }
